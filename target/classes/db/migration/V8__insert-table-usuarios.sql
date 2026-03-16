10a457774bee4b5a0f16e46c0c49320e5dfc2be2
GustavoG <126207720+ggSOS@users.noreply.github.com>
2026-03-15 21:42:58 -0300

    Target remove

--- target/classes/db/migration/V8__insert-table-usuarios.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `auto_escola`.`usuarios`
-(`id`, `login`, `senha`, `perfil`)
-VALUES
-('1', 'adminLogin', '$2a$10$nGkOrbgR4iGHOPbR/ikPdO.RfhqB/XoH6//lTfjO6Ipmiy2G7iQ36', 'ADMIN');
-
-INSERT INTO `auto_escola`.`usuarios`
-(`id`, `login`, `senha`, `perfil`)
-VALUES
-('2', 'commonLogin', '$2a$10$Riola5Oeo6aNdG7MU11OrexuBSHO7vgOZa8jA3G2OpQbqGheqYODq', 'USER');
