6ddaabd9ce3bb9a880026d35ca3a93a80e003bfb
GustavoG <126207720+ggSOS@users.noreply.github.com>
2026-04-25 20:27:32 -0300

    Delete target directory

--- target/classes/db/migration/V4__create-table-usuarios.sql
+++
@@ -1,8 +0,0 @@
--- dialect:mysql
-
-Create Table usuarios(
-    id              BigInt          Not Null    Auto_increment,
-    login           VarChar(100)    Not Null    Unique,
-    senha           VarChar(255)    Not Null,
-    Primary Key(id)
-);
